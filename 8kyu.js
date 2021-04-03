d1c968d24cab4d819e2b672c7c18f57b294e11bd
Max Matkovski <74901101+maxmatkovski@users.noreply.github.com>
2021-04-03 14:34:42 -0700

    Update 8kyu.js
    switch statement practice

--- 8kyu.js
+++ 8kyu.js
@@ -116,3 +116,32 @@ function highAndLow(numberList) {
 
 /////////////////////////////////////////////////////////////////////////////////////
 
+// codewars.com/kata/572059afc2f4612825000d8a/solutions/javascript
+
+function howManydays(month){
+    switch (month) {
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        days = 31;
+        break;
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+        days = 30;
+        break;
+      case 2:
+        days = 28;
+        break;
+    }
+    return days;
+  }
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
